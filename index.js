b102c9a0d3f8d082273cbd86a698131fea837cc3
Mansoor Sahito <mansoorsahito601@gmail.com>
2025-06-22 08:49:44 +0500

    UseShared Animation

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ import App from './App';
 import { name as appName } from './app.json';
 import Home from './src/screens/Home';
 import First from './src/AnimationScreens/First';
+import { useSharedValue } from 'react-native-reanimated';
 
-AppRegistry.registerComponent(appName, () => First);
+AppRegistry.registerComponent(appName, () => useSharedValue);
